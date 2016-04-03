b9b826838b5d9fd30be816390a7a451ddd14977f
Evan Turner <evturn@gmail.com>
2016-04-03 17:38:08 -0400

    Map over stars on an interval and adjust coordinates

--- dist/bundle.js
+++ dist/bundle.js
@@ -10,13 +10,27 @@
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
+  var SPEED = 40;
   var STAR_NUMBER = 250;
+
   var StarStream = Rx.Observable.range(1, STAR_NUMBER).map(function () {
     return {
       x: parseInt(Math.random() * canvas.width),
       y: parseInt(Math.random() * canvas.height),
       size: Math.random() * 3 + 1
     };
+  }).toArray().flatMap(function (starArray) {
+    return Rx.Observable.interval(SPEED).map(function () {
+      starArray.forEach(function (star) {
+        if (star.y >= canvas.height) {
+          star.y = 0;
+        }
+
+        star.y += 3;
+      });
+
+      return starArray;
+    });
   });
 
 }());
--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,31 @@ document.body.appendChild(canvas);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+
 const SPEED = 40;
 const STAR_NUMBER = 250;
-const StarStream = Rx.Observable.range(1, STAR_NUMBER)
+
+const StarStream = Rx.Observable
+  .range(1, STAR_NUMBER)
   .map(() => ({
       x: parseInt(Math.random() * canvas.width),
       y: parseInt(Math.random() * canvas.height),
       size: Math.random() * 3 + 1
     })
-  );
+  )
+  .toArray()
+  .flatMap(starArray => {
+    return Rx.Observable
+      .interval(SPEED)
+      .map(() => {
+        starArray.forEach(star => {
+          if (star.y >= canvas.height) {
+            star.y = 0;
+          }
+
+          star.y += 3;
+        });
+
+        return starArray;
+      });
+  });
