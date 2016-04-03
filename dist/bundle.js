4345c55dc01f2b7c6589017ffe50e51aab33e18c
Evan Turner <evturn@gmail.com>
2016-04-03 17:20:24 -0400

    Append canvas to document body

--- dist/bundle.js
+++ dist/bundle.js
@@ -3,4 +3,11 @@
 
 	console.log('sup with them cookies?');
 
+	var canvas = document.createElement('canvas');
+	var ctx = canvas.getContext('2d');
+
+	document.body.appendChild(canvas);
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
 }());
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 console.log('sup with them cookies?');
+
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+
+document.body.appendChild(canvas);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
