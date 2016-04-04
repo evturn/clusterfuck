28919dc9208c2fa6e0acfa42c419087e047a4839
Evan Turner <evturn@gmail.com>
2016-04-04 14:18:17 -0400

    Update Opponents Observable

--- src/js/opponents.js
+++ src/js/opponents.js
@@ -1,18 +1,33 @@
 import { canvas } from './canvas';
+import { ENEMY_FREQ, ENEMY_SHOOTING_FREQ } from './constants';
 
-const ENEMY_FREQ = 1500;
+function isVisible(obj) {
+  return obj.x > -40 && obj.x < canvas.width + 40 && obj.y > -40 && obj.y < canvas.height + 40;
+}
 
 const Opponents = Rx.Observable
   .interval(ENEMY_FREQ)
   .scan(enemyArray => {
     const enemy = {
       x: parseInt(Math.random() * canvas.width),
-      y: -30
+      y: -30,
+      shots: []
     };
+
+    Rx.Observable
+      .interval(ENEMY_SHOOTING_FREQ)
+      .subscribe(() => {
+        enemy.shots.push({
+          x: enemy.x,
+          y: enemy.y
+        });
+
+        enemy.shots = enemy.shots.filter(isVisible);
+      });
 
     enemyArray.push(enemy);
 
-    return enemyArray;
+    return enemyArray.filter(isVisible);
   }, []);
 
 export default Opponents;
